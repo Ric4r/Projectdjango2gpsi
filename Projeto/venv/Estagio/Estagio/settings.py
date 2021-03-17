e9b1d5b842e7bb701ba235315841aded3d829820
Ric4r <1019071@alunos.ruizcosta.edu.pt>
2021-03-17 18:08:44 +0000

    Alterção do css

--- Projeto/venv/Estagio/Estagio/settings.py
+++ Projeto/venv/Estagio/Estagio/settings.py
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_URL=(
+    os.path.join(BASE_DIR,'css'),
+)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
